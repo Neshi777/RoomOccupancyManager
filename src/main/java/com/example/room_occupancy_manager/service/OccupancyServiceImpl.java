94f00ede56bb4e2786160835f86b0fefd597dcda
Marcin Miechowiak <marcin.miechowiak@gmail.com>
2024-06-23 21:55:37 +0200

    Implemented main calculate occupancy logic

--- src/main/java/com/example/room_occupancy_manager/service/OccupancyServiceImpl.java
+++ src/main/java/com/example/room_occupancy_manager/service/OccupancyServiceImpl.java
@@ -1,15 +1,89 @@
 package com.example.room_occupancy_manager.service;
 
+import com.example.room_occupancy_manager.data_loader.JsonDataLoader;
 import com.example.room_occupancy_manager.model.OccupancyRequest;
 import com.example.room_occupancy_manager.model.OccupancyResponse;
+import com.example.room_occupancy_manager.model.PotentialOccupancies;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-public class OccupancyServiceImpl implements OccupancyService{
+@Slf4j
+public class OccupancyServiceImpl implements OccupancyService {
+    static final double PREMIUM_PRICE_THRESHOLD = 100;
+    private final JsonDataLoader jsonDataLoader;
+    @Value("${potential.occupancies.file}")
+    private String potentialOccupanciesFilePath;
+
+    @Autowired
+    public OccupancyServiceImpl(JsonDataLoader jsonDataLoader) {
+        this.jsonDataLoader = jsonDataLoader;
+    }
 
     @Override
     public OccupancyResponse calculateOccupancy(OccupancyRequest request) {
-        return new OccupancyResponse();
+        validateInput(request);
+
+        PotentialOccupancies potentialOccupancies = jsonDataLoader.loadPotentialOccupanciesFromFile(potentialOccupanciesFilePath);
+        List<Double> premiumOffers = filterAndSortOffers(potentialOccupancies.getOffers(), true);
+        List<Double> economyOffers = filterAndSortOffers(potentialOccupancies.getOffers(), false);
+        int freePremiumRooms = request.getFreePremiumRooms();
+        int freeEconomyRooms = request.getFreeEconomyRooms();
+
+        int usedPremiumRooms = Math.min(freePremiumRooms, premiumOffers.size());
+        double premiumRevenue = premiumOffers.stream().limit(usedPremiumRooms).mapToDouble(Double::doubleValue).sum();
+        int remainingPremiumRooms = freePremiumRooms - usedPremiumRooms;
+        int economyOffersSurplus = economyOffers.size() - freeEconomyRooms;
+        int offersToBeUpgraded = 0;
+
+        if (economyOffersSurplus > 0 && remainingPremiumRooms > 0) {
+            offersToBeUpgraded = Math.min(remainingPremiumRooms, economyOffersSurplus);
+            premiumRevenue += calculateUpgradedRevenue(economyOffers, offersToBeUpgraded);
+            usedPremiumRooms += offersToBeUpgraded;
+        }
+
+        int usedEconomyRooms = Math.min(freeEconomyRooms, economyOffers.size());
+        double economyRevenue = economyOffers.stream().skip(offersToBeUpgraded).limit(usedEconomyRooms).mapToDouble(Double::doubleValue).sum();
+
+        return createResponse(usedPremiumRooms, premiumRevenue, usedEconomyRooms, economyRevenue);
+    }
+
+    private void validateInput(OccupancyRequest request) {
+        if (request.getFreePremiumRooms() < 0 || request.getFreeEconomyRooms() < 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Number of rooms cannot be negative");
+        }
+    }
+
+    private List<Double> filterAndSortOffers(List<Double> offers, boolean premium) {
+        return offers.stream()
+                .filter(offer -> (premium && offer >= PREMIUM_PRICE_THRESHOLD) || (!premium && offer < PREMIUM_PRICE_THRESHOLD))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
+
+    private double calculateUpgradedRevenue(List<Double> economyOffers, int offersToBeUpgraded) {
+        return economyOffers.stream()
+                .limit(offersToBeUpgraded)
+                .mapToDouble(Double::doubleValue)
+                .sum();
+    }
+
+    private OccupancyResponse createResponse(int usedPremiumRooms, double premiumRevenue, int usedEconomyRooms, double economyRevenue) {
+        OccupancyResponse response = new OccupancyResponse();
+        response.setUsagePremium(usedPremiumRooms);
+        response.setRevenuePremium(premiumRevenue);
+        response.setUsageEconomy(usedEconomyRooms);
+        response.setRevenueEconomy(economyRevenue);
+        log.info("Calculated occupancy: {}", response);
+        return response;
     }
 
 }
--- src/test/java/com/example/room_occupancy_manager/service/OccupancyServiceImplTest.java
+++ src/test/java/com/example/room_occupancy_manager/service/OccupancyServiceImplTest.java
@@ -1,7 +1,140 @@
 package com.example.room_occupancy_manager.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.room_occupancy_manager.data_loader.JsonDataLoader;
+import com.example.room_occupancy_manager.model.OccupancyRequest;
+import com.example.room_occupancy_manager.model.OccupancyResponse;
+import com.example.room_occupancy_manager.model.PotentialOccupancies;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.lenient;
+
+@ExtendWith(MockitoExtension.class)
 class OccupancyServiceImplTest {
+
+    @Mock
+    private JsonDataLoader jsonDataLoader;
+
+    @InjectMocks
+    private OccupancyServiceImpl occupancyService;
+
+    @BeforeEach
+    void setUp() {
+
+        // Prepare mock data
+        List<Double> offers = Arrays.asList(23.0, 45.0, 155.0, 374.0, 22.0, 99.99, 100.0, 101.0, 115.0, 209.0);
+        PotentialOccupancies potentialOccupancies = new PotentialOccupancies();
+        potentialOccupancies.setOffers(offers);
+
+        // Mock the behavior of jsonUtil
+        lenient().when(jsonDataLoader.loadPotentialOccupanciesFromFile(null)).thenReturn(potentialOccupancies);
+
+    }
+
+    @Test
+    void testCalculateOccupancy_scenario1() {
+        // Given
+        int freePremiumRooms = 3;
+        int freeEconomyRooms = 3;
+        OccupancyRequest request = new OccupancyRequest(freePremiumRooms, freeEconomyRooms);
+
+        // When
+        OccupancyResponse response = occupancyService.calculateOccupancy(request);
+
+        // Then
+        assertEquals(3, response.getUsagePremium());
+        assertEquals(738.0, response.getRevenuePremium(), 0.01);
+        assertEquals(3, response.getUsageEconomy());
+        assertEquals(167.99, response.getRevenueEconomy(), 0.01);
+    }
+
+    @Test
+    void testCalculateOccupancy_scenario2() {
+        // Given
+        int freePremiumRooms = 7;
+        int freeEconomyRooms = 5;
+        OccupancyRequest request = new OccupancyRequest(freePremiumRooms, freeEconomyRooms);
+
+        // When
+        OccupancyResponse response = occupancyService.calculateOccupancy(request);
+
+        // Then
+        assertEquals(6, response.getUsagePremium());
+        assertEquals(1054.0, response.getRevenuePremium(), 0.01);
+        assertEquals(4, response.getUsageEconomy());
+        assertEquals(189.99, response.getRevenueEconomy(), 0.01);
+    }
+
+    @Test
+    void testCalculateOccupancy_scenario3() {
+        // Given
+        int freePremiumRooms = 2;
+        int freeEconomyRooms = 7;
+        OccupancyRequest request = new OccupancyRequest(freePremiumRooms, freeEconomyRooms);
+
+        // When
+        OccupancyResponse response = occupancyService.calculateOccupancy(request);
+
+        // Then
+        assertEquals(2, response.getUsagePremium());
+        assertEquals(583.0, response.getRevenuePremium(), 0.01);
+        assertEquals(4, response.getUsageEconomy());
+        assertEquals(189.99, response.getRevenueEconomy(), 0.01);
+    }
+
+    @Test
+    void testCalculateOccupancy_scenario4() {
+        // Given
+        int freePremiumRooms = 7;
+        int freeEconomyRooms = 1;
+        OccupancyRequest request = new OccupancyRequest(freePremiumRooms, freeEconomyRooms);
+
+        // When
+        OccupancyResponse response = occupancyService.calculateOccupancy(request);
+
+        // Then
+        assertEquals(7, response.getUsagePremium());
+        assertEquals(1153.99, response.getRevenuePremium(), 0.01);
+        assertEquals(1, response.getUsageEconomy());
+        assertEquals(45.00, response.getRevenueEconomy(), 0.01);
+    }
+
+    @Test
+    void testCalculateOccupancy_negativePremiumRooms() {
+        // Given
+        int freePremiumRooms = -1;
+        int freeEconomyRooms = 3;
+        OccupancyRequest request = new OccupancyRequest(freePremiumRooms, freeEconomyRooms);
+
+        // When & Then
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> occupancyService.calculateOccupancy(request));
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatusCode());
+        assertEquals("Number of rooms cannot be negative", exception.getReason());
+    }
+
+
+    @Test
+    void testCalculateOccupancy_negativeEconomyRooms() {
+        // Given
+        int freePremiumRooms = 3;
+        int freeEconomyRooms = -1;
+        OccupancyRequest request = new OccupancyRequest(freePremiumRooms, freeEconomyRooms);
+
+        // When & Then
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> occupancyService.calculateOccupancy(request));
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatusCode());
+        assertEquals("Number of rooms cannot be negative", exception.getReason());
+    }
 
 }
